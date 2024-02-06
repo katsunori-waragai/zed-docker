16233f28fededc909fdcc07b94020d09827f6046
katsunori-waragai <huyuhiko1128@gmail.com>
2024-02-06 14:40:23 +0900

    DEPTH

--- capture_view.py
+++ capture_view.py
@@ -62,7 +62,7 @@ def main():
             data = image.get_data()  # 戻り値が配列になる。
             print(f"{image.get_data_type()=}")
             print(f"{image.get_channels()=}")
-        q    print(f"{image.get_height()=}")
+            print(f"{image.get_height()=}")
             print(f"{image.get_width()=}")
             print(f"{image.get_infos()=}")
 
