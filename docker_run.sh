3953c2e5b5ca07ffd109da605f13deca6e2b41d6
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-02-06 11:01:59 +0900

    use /home/waragai

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
 	--privileged \
-	-v ~/github/yolox-docker/YOLOX_outputs:/root/YOLOX/YOLOX_outputs \
-	-v ~/github/yolox-docker:/root/YOLOX/yolox-docker \
+	-v ${HOME}/github/zed-sdk/:/usr/local/zed/zed-sdk \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix zed-docker:100
