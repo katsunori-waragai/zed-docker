2f4a2ccac022a4921e35459083ce20211a753da0
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-11-20 14:18:17 +0900

    git clone inside Dcker

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
-	--privileged \
-	-v ${HOME}/github/zed-sdk/:/usr/local/zed/zed-sdk \
+xhost +
+docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix zed-docker:100
